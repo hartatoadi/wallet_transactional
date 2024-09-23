e3ece247824b9132b9e0d51479c20eb016289cb7
Hartato Adi <hartatoadi@gmail.com>
2024-09-23 23:44:16 +0700

    activating bcrypt and add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }, uniqueness: true
   has_one :wallet, as: :walletable, dependent: :destroy
   after_create :create_wallet
 
